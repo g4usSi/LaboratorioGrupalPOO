08d590ab763c43c0659a062dbf72c661f65f138d
SheilynG <sheilyndelacruzsanchez@gmail.com>
2024-08-23 10:14:05 -0500

    .

--- Laboratorio#4_Grupal/ContactoCorporativo.cs
+++ Laboratorio#4_Grupal/ContactoCorporativo.cs
@@ -55,19 +55,6 @@ namespace Laboratorio_4_Grupal
 
        
 
-            Console.Write("Ingrese el nombre de la empresa: ");
-            string empresaBuscar = Utilidades.LlenarString();
-
-            foreach (var contactoBuscar in listaContactosCorporativos)
-            {
-                if (contactoBuscar.NombreEmpresa == empresaBuscar)
-                {
-                    return contactoBuscar;
-                }
-            }
-
-            return null;
-        }
 
     }
 }
